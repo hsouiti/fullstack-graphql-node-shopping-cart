01363e92033a547c9f5ef1cf6b9a7f402725a364
hassan <h.souiti@gmail.com>
2019-10-02 18:57:51 +0100

    add mongodb & graphql user schema

---
+++ server/graphql/typeDefs/user.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express')
+
+module.exports = gql`
+  extend type Query {
+    getUser(id: ID!): User
+    getUsers: [User!]!
+  }
+
+  extend type Mutation {
+    signUp(signupFields: SignupFields): AuthPayload!
+    signIn(email: String! password:String!): AuthPayload!
+    signOut: Boolean
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    username: String!
+    name: String!
+    role: String!
+    createdAt: String!
+  }
+
+  input SignupFields {
+    email: String!
+    name: String!
+    username: String!
+    password: String!
+    confirmPassword: String!
+    role: String!
+  }
+   
+`
---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    validate: {
+      validator: email => User.isUnique({ email }),
+      message: 'Email already exit!'
+    }
+  },
+  username: {
+    type: String,
+    validate: {
+      validator: username => User.isUnique({ username }),
+      message: 'Username already exit!'
+    }
+  },
+  name: {
+    type: String,
+    default: 'user'
+  },
+  address: String,
+  password: String,
+  role: String
+}, { timestamps: true }
+)
+
+
+userSchema.statics.isUnique = async function (option) {
+  return await this.where(option).countDocuments() === 0
+}
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password)
+}
+
+
+const User = model('User', userSchema)
+module.exports = User
+
