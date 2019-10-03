da1c89cdad9a9900d63171a138145386ef252f7f
hassan <h.souiti@gmail.com>
2019-10-03 22:44:51 +0100

    Add product resolver

---
+++ server/graphql/resolvers/product.js
@@ -0,0 +1,23 @@
+const { UserInputError } = require('apollo-server-express')
+const Product = require('../../models/product')
+
+module.exports = {
+  Query: {
+    getProduct: async (_, { id }, { req }) => {
+      return Product.findById(id)
+    },
+    getProducts: async (_, args) => {
+      try {
+        return await Product.find({}).sort({ createdAt: -1 })
+      } catch (err) {
+        throw new UserInputError(err)
+      }
+    }
+  },
+  Mutation: {
+    addProduct: async (_, { productFields }, { req }) => {
+      const product = await Product.create({ ...productFields })
+      return product
+    }
+  }
+}
