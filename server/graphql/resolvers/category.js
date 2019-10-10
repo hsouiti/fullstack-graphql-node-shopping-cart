7fa53b32b9414093fcf72884683ebbcefa40abf6
hassan <h.souiti@gmail.com>
2019-10-10 21:26:16 +0100

    category resolver

--- server/graphql/resolvers/category.js
+++ server/graphql/resolvers/category.js
@@ -1,10 +1,13 @@
 const { UserInputError } = require('apollo-server-express')
 const Category = require('../../models/category')
+const Product = require('../../models/product')
+
+const { isLoggedIn } = require('../../utils/auth')
 
 module.exports = {
   Query: {
-    getCategory: async (_, { id }, { req }) => {
-      return Category.findById(id)
+    getCategory: async (root, { id }, { req }) => {
+      return await Category.findById(id)
     },
     getCategories: async (_, args) => {
       try {
@@ -14,11 +17,47 @@ module.exports = {
       }
     }
   },
+  Category: {
+    products: async (root, __, { req }) => {
+      try {
+        return await Product.find({ categoryId: root._id })
+      } catch (err) {
+        throw new UserInputError(err)
+      }
+    },
+  },
   Mutation: {
     addCategory: async (_, { categoryFields }, { req }) => {
+      // Check if user authenticated
+      await isLoggedIn(req)
+
       const category = await Category.create({ ...categoryFields })
       return category
-    }
+    },
+    updateCategory: async (_, { id, categoryFields }, { req }) => {
+      // Check if user authenticated
+      await isLoggedIn(req)
+      try {
+        const category = await Category.findById(id)
+        if (category) {
+          category.name = categoryFields.name
+          return await category.save()
+        }
+      } catch (err) {
+        throw new UserInputError(err)
+      }
+    },
+    deleteCategory: async (_, { id }, { req }) => {
+      // TODO: Check first if the category is empty 
+      // not assigned to any product before delete it
 
+      // Check if user authenticated
+      await isLoggedIn(req)
+      try {
+        await Category.findByIdAndRemove(id)
+      } catch (err) {
+        throw new UserInputError(err)
+      }
+    }
   }
 }
