7111b790fe7fbe8da05a949a9dbb9f4082477fb5
hassan <h.souiti@gmail.com>
2019-10-12 18:50:19 +0100

    cart resolver update

--- server/graphql/resolvers/cart.js
+++ server/graphql/resolvers/cart.js
@@ -39,37 +39,66 @@ module.exports = {
         const cart = await Cart.findOne({ userId: cartItems.userId })
         if (cart) {
           const product = cart.items.find(item => JSON.stringify(item.productId) === JSON.stringify(cartItems.productId))
-
           if (product) {
             product.quantity += cartItems.quantity
+            if (product.quantity === 0) {
+              const newItems = cart.items.filter(item => item.id === cartItems.id)
+              cart.items = newItems
+            }
             cart.save()
           } else {
-            cart.items.push({
-              productId: cartItems.productId,
-              quantity: cartItems.quantity,
-              price: cartItems.price
-            })
-            cart.save()
+            const product = await Product.findOne({ _id: cartItems.productId })
+            if (product) {
+              cart.items.push({
+                productId: cartItems.productId,
+                quantity: cartItems.quantity,
+                price: product.price
+              })
+              cart.save()
+            }
           }
           return cart
         } else {
-          newCart = new Cart({
-            userId: cartItems.userId,
-            items: {
-              productId: cartItems.productId,
-              quantity: cartItems.quantity,
-              price: cartItems.price
-            }
-          })
-          await newCart.save()
-          return newCart
+          const product = await Product.findOne({ _id: cartItems.productId })
+          if (product) {
+            newCart = new Cart({
+              userId: cartItems.userId,
+              items: {
+                productId: cartItems.productId,
+                quantity: cartItems.quantity,
+                price: product.price
+              }
+            })
+            await newCart.save()
+            return newCart
+          }
+
         }
 
       } catch (err) {
         throw new UserInputError(err)
       }
     },
-    /* removeFromCart: async (_, { cartItems }, { req }) => {
-    } */
+    removeFromCart: async (_, { cartItems }, { req }) => {
+      try {
+        const cart = await Cart.findOne({ userId: cartItems.userId })
+
+        if (cart) {
+          const productToRemove = cart.items.find(item => {
+            return JSON.stringify(item.productId) === JSON.stringify(cartItems.productId)
+          })
+          if (productToRemove) {
+            const newItems = cart.items.filter(item => {
+              return item.productId !== productToRemove.productId
+            })
+            cart.items = newItems
+            const updatedCart = await cart.save()
+            return updatedCart
+          }
+        }
+      } catch (err) {
+        throw new UserInputError(err)
+      }
+    }
   }
 }
